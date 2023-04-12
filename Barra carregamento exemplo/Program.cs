dbe9b74eacca3c81999d088e2091b0b4153ce89d
merbach020202 <eduardo.brenn2006@gmail.com>
2023-04-12 10:58:55 -0300

    12/04

---
+++ Barra carregamento exemplo/Program.cs
@@ -0,0 +1,39 @@
+﻿//Console.WriteLine($"Carregando...............");
+
+static void BarraCarregamento(string texto, int quantidadePontinhos, int tempo)
+{
+    
+    Console.Write(texto);
+
+    for (int i = 0; i < quantidadePontinhos; i++)
+    {
+        Console.Write($".");
+        Thread.Sleep(tempo);
+    }
+    
+
+}
+
+Console.ForegroundColor = ConsoleColor.Black;
+BarraCarregamento("Testando",10,500);
+
+Console.ForegroundColor = ConsoleColor.Blue;
+BarraCarregamento("Finalizando",10,400);
+
+Console.ForegroundColor = ConsoleColor.DarkCyan;
+BarraCarregamento("Aguarde",15,300);
+
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+BarraCarregamento("Cadastrando",15,200);
+
+Console.ForegroundColor = ConsoleColor.Green;
+BarraCarregamento("Logando",20,100);
+Console.ResetColor();
+
+
+
+
+
+
+
+
---
+++ Funcoes das 4 operacoes/Program.cs
@@ -0,0 +1,116 @@
+﻿//funções(Defina um codigo uma vez e use-o varias vezes)
+// VOID = Nao possui valor de retorno
+// MyMethod() = é o nome do método (Usar PascalCase)
+
+// static void MyMethod()
+// {
+//bloco de codigo
+// }
+
+
+
+//criar um metodo para exibir o nome de uma pessoa, sobrenome e exibir "Bom dia Fulano de tal"
+
+// static void Saudacao()
+// {
+//     Console.WriteLine($"Informe o nome da pessoa: ");
+//     string nome = Console.ReadLine();
+
+//     Console.WriteLine($"Informe o sobrenome da pessoa: ");
+//     string sobrenome = Console.ReadLine();
+
+//     Console.WriteLine($"Bom dia {nome} {sobrenome}!");
+// }
+
+
+// Saudacao();
+
+
+
+//criar um metodo para somar 2 numeros
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine($"Operação a ser efetuada: SOMA");
+Console.ResetColor();
+
+static float Somar(float n1, float n2)
+{
+    return (n1 + n2);
+}
+
+Console.WriteLine($"Informe o 1º numero: ");
+float primeiroNumero = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"Informe o 2º numero: ");
+float segundoNumero = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"A soma de {primeiroNumero} + {segundoNumero} é {Somar(primeiroNumero, segundoNumero)}");
+
+//Somar(primeiroNumero, segudoNumero)
+
+
+
+//Criar os metodos para as demais operacoes matematicas (subtracao, multiplicacao e divisao)
+//Receber os numeros e exibir os resultados
+
+
+//DIVISAO
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine($"Operação a ser efetuada: Divisao");
+Console.ResetColor();
+
+static float Divisao(float n1, float n2)
+{
+    return (n1 / n2);
+}
+
+Console.WriteLine($"Informe o 1º numero:");
+float n1 = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"Informe o 2º numero:");
+float n2 = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"A divisao do {n1} e do {n2} = {Divisao(n1, n2)}");
+
+
+
+//MULTIPLICACAO
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine($"Operação a ser efetuada: MULTIPLICAÇÃO");
+Console.ResetColor();
+
+static float Multiplicacao(float n1, float n2)
+{
+    return (n1 * n2);
+}
+
+Console.WriteLine($"Informe o 1º numero:");
+float numero1 = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"Informe o 2º numero:");
+float numero2 = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"A multiplicacao do {numero1} e do {numero2} = {Multiplicacao(numero1, numero2)}");
+
+
+
+//SUBTRACAO
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine($"Operação a ser efetuada: SUBTRAÇÃO");
+Console.ResetColor();
+
+static float Subtracao(float n1, float n2)
+{
+    return (n1 - n2);
+}
+
+Console.WriteLine($"Informe o 1º numero:");
+float Numero = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"Informe o 2º numero:");
+float Numero2 = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"A subtração do {Numero} e do {Numero2} = {Subtracao(Numero, Numero2)}");
+
+
+
