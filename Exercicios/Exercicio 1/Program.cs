be07ec8b40e14e0374261bc0d4c2ce4067448a19
merbach020202 <eduardo.brenn2006@gmail.com>
2023-04-13 11:01:01 -0300

    13/04

---
+++ Exercicios/Exercicio 1/Program.cs
@@ -0,0 +1,43 @@
+﻿
+Console.WriteLine($"Digite o ano de seu nascimento: ");
+int ano = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Digite o ano atual: ");
+int anoAtual = int.Parse(Console.ReadLine());
+
+
+
+int resultado = anoAtual - ano;
+
+if (resultado < 16)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"você não tem a idade mínima para votar!");
+    Console.ResetColor();
+}
+else if (resultado >= 16 && resultado < 18)
+{
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine($"Seu voto é opcional!");
+    Console.ResetColor();
+}
+
+else if (resultado >= 18 && resultado < 70)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"você deve votar!");
+    Console.ResetColor();
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine($"Seu voto é opcional!");
+    Console.ResetColor();
+}
+
+
+
+
+
+
+
---
+++ Exercicios/Exercicio 2/Program.cs
@@ -0,0 +1,83 @@
+﻿//Um posto está vendendo combustíveis com a seguinte tabela de descontos:
+//Álcool:
+//. até 20 litros, desconto de 3% por litro Álcool
+//. acima de 20 litros, desconto de 5% por litro
+//Gasolina:
+//. até 20 litros, desconto de 4% por litro Gasolina
+//. acima de 20 litros, desconto de 6% por litro
+
+//Escreva um algoritmo que leia o número de litros vendidos e o tipo de combustível (codificado
+//da seguinte forma: A-álcool, G-gasolina), calcule e imprima o valor a ser pago pelo cliente
+//sabendo-se que o preço do litro da gasolina é R$ 5,30 e o preço do litro do álcool é R$ 4,90.
+//Dica: utilize switch case e funções/métodos para otimizar o algorítimo.
+
+static float CombustivelG(float a)
+{
+    if (a <= 20)
+    {
+        return (float)(((5.30 / 100) * 4) * a);
+    }
+    else
+    {
+        return (float)(((5.30 / 100) * 6) * a);
+    }
+}
+
+
+Console.WriteLine(@$"Digite o tipo de combustivel que você deseja escolher:
+g - gasolina
+a - alcool
+");
+char tipoCombustivel = char.Parse(Console.ReadLine().ToLower());
+
+
+Console.WriteLine($"Digite a quantidade de litros vendidos: ");
+int quantidadeLitros = int.Parse(Console.ReadLine());
+
+if (tipoCombustivel == 'g')
+{
+    float valorFinal = CombustivelG(quantidadeLitros);
+
+    Console.WriteLine($"O valor a ser pago é de: {valorFinal}");
+
+}
+
+
+
+
+
+else if (tipoCombustivel == 'a')
+{
+    Console.WriteLine($"Digite a quantidade de litros vendidos: ");
+    float precoA = float.Parse(Console.ReadLine());
+}
+
+
+
+
+
+else
+{
+    Console.WriteLine($"Digite novamemte o tipo de combustivel desejado!");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
